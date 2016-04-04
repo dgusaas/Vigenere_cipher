8555d7863db2a5b2441e9fbef506666487d867aa
DjGusaas <gusaa004@morris.umn.edu>
2016-04-04 14:52:21 -0500

    finished decode/encode

--- cipher.rb
+++ cipher.rb
@@ -1,10 +1,12 @@
 #!/usr/local/bin/ruby -w
+$key = "key"
 
-$key = "beck"
+def c_to_n(c)
+	return c.downcase.ord - 97
+end
 
-def alpha_to_num(char)
-	alpha = 'a'..'z'
-	puts alpha.find_index(char.downcase)
+def n_to_c(n)
+	return (n + 97).chr
 end
 
 def encode(str)
@@ -12,8 +14,45 @@ def encode(str)
 	while (str.length >= tmp.length)
 		tmp += $key
 	end
-	return tmp[0..str.length-1]
+	tmp_arr = tmp[0..str.length-1].split(//)
+	str_arr = str.split(//)
+	result = []
+	tmp_arr.each_with_index do |x, i|
+		result << (c_to_n(tmp_arr[i]) + c_to_n(str_arr[i])) % 26
+	end
+	result = result.map { |x| n_to_c(x) }
+	return result.join()
 end
 
-a = encode("dog")
-puts a
+def decode(str)
+	tmp = ""
+	while (str.length >= tmp.length)
+		tmp += $key
+	end
+	tmp_arr = tmp[0..str.length-1].split(//)
+	str_arr = str.split(//)
+	result = []
+	tmp_arr.each_with_index do |x, i|
+		result << (c_to_n(str_arr[i]) - c_to_n(tmp_arr[i])) % 26
+	end
+	result = result.map { |x| n_to_c(x) }
+	return result.join()
+end
+
+def key_elim(str, offset)
+	str_arr = str.split(//)
+	off_arr = str_arr.map { |x| n_to_c(c_to_n(x) + offset)}
+	result = []
+	str_arr.each_with_index do |x, i|
+		result << (c_to_n(str_arr[i]) - c_to_n(off_arr[i])) % 26
+	end
+	result = result.map { |x| n_to_c(x) }
+	return result.join()
+end
+
+puts key_elim("abc", 3)
+
+# puts encode("dog") # returns "nse"
+# puts decode("nse") # returns "dog"
+# puts encode("function") # returns "pylmxgyr"
+# puts decode("pylmxgyr") # returns "function"
