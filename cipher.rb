2e3e951a0dbfc5fd485d3501a9afdc1233c65ff6
DjGusaas <gusaa004@morris.umn.edu>
2016-04-05 17:44:54 -0500

    made some small changes

--- cipher.rb
+++ cipher.rb
@@ -1,5 +1,6 @@
 #!/usr/local/bin/ruby -w
 $key = "key"
+$target = "coliwicojqkwbjplvwffjvapvjuikicomitxrxnemtbefickmpeitfoohesnojwxcgrddvkhitesnnafxjhhfhxxvxbmxvgpvjuikgcemifgsbapagoqympzgvmhggzzclgksdjqgthbytkgubbshlcnnspxufwxnrfbytkgusjtrbbhjxorqijqdxfexstmwtbsoxjjbmvhfjvyvmssnhvtdqrrithnhgjtacnvfhcsxrnrhirwcgroxxjbbhvstxoimmumwolxnrwsnqgfpfamvpotrrvascuicdrflioussjrfxodqwgiosjxgwmjwkgfpfaathihqxkmghqsumiqxrvasgusqksbpitemjfmuaseclgfcsasyoojwpabvbmwqnuicxqucsasyyfpvqaucptwunfdneuxcgbstkcxbstkcxostmvfusumzfwstxtpaxjxfbaicgrsjhktbuvekwsoflqfhinepzsmbrcfsmnrqksojqgestblgksgxvgossvstx"
 
 def c_to_n(c)
 	return c.downcase.ord - 97
@@ -39,20 +40,50 @@ def decode(str)
 	return result.join()
 end
 
-def key_elim(str, offset)
-	str_arr = str.split(//)
-	off_arr = str_arr.map { |x| n_to_c(c_to_n(x) + offset)}
-	result = []
-	str_arr.each_with_index do |x, i|
-		result << (c_to_n(str_arr[i]) - c_to_n(off_arr[i])) % 26
-	end
-	result = result.map { |x| n_to_c(x) }
-	return result.join()
-end
-
-puts key_elim("abc", 3)
-
 # puts encode("dog") # returns "nse"
 # puts decode("nse") # returns "dog"
 # puts encode("function") # returns "pylmxgyr"
 # puts decode("pylmxgyr") # returns "function"
+
+def blah(str)
+	str_arr = str.split(//)
+	guess = 6
+	start = 0
+	tmp_arr = []
+	while (start != 6)
+		tmp_arr << str_arr[0][start]
+		for i in start..str_arr.length
+			if (i + 1) % guess == 0
+				tmp_arr << str_arr[start][i]
+			end
+		end
+		start += 1
+	end
+	puts tmp_arr[0][1]
+end
+
+blah("daltondaltondaltondalton")
+
+# def generate_structure(str)
+# 	str_arr = str.split(//)
+# 	str_fake = []
+# 	str_arr.each_with_index do |x, i|
+# 		str_fake[i] = []
+# 		for j in 0..(str_arr.length -  1) - i
+# 			str_fake[i][j] = str_arr[j]
+# 		end
+# 	end
+# 	puts str_fake[2][3]
+# 	return str_fake
+# end
+#
+# # puts generate_fake_hash("key").instance_of? nil
+#
+# def coincidences(str)
+# 	tmp = generate_fake_hash(str)
+# 	length = str.length
+#   counts = []
+# 	for i 0..length - 1
+# 		arr[i][j]
+# 	end
+# end
